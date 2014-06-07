d2cfa8d899241df0451314c6323194e8cfc97c4f
ajla24 <asakic1@etf.unsa.ba>
2014-06-07 18:32:31 +0200

    ..

--- Projekat/Projekat/Form1.Designer.cs
+++ Projekat/Projekat/Form1.Designer.cs
@@ -40,10 +40,10 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button2.ForeColor = System.Drawing.Color.Snow;
-            this.button2.Location = new System.Drawing.Point(485, 406);
+            this.button2.Location = new System.Drawing.Point(539, 450);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 43);
+            this.button2.Size = new System.Drawing.Size(130, 60);
             this.button2.TabIndex = 1;
             this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = false;
@@ -55,7 +55,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(629, 480);
+            this.ClientSize = new System.Drawing.Size(698, 536);
             this.Controls.Add(this.button2);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
